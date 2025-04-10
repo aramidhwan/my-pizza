991b89103c5bd1d31d1f560158e77f88da50fefb
Shin, Seung Hwan <aramidhwan@naver.com>
2025-04-10 09:52:35 +0900

    HttpRedirectServer.java : System.out > log.info 로 변경

--- gateway/src/main/java/com/study/mypizza/gateway/config/HttpRedirectServer.java
+++ gateway/src/main/java/com/study/mypizza/gateway/config/HttpRedirectServer.java
@@ -1,12 +1,14 @@
 package com.study.mypizza.gateway.config;
 
 import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import reactor.netty.DisposableServer;
 import reactor.netty.http.server.HttpServer;
 
 @Component
+@Slf4j
 public class HttpRedirectServer {
 
     @Value("${mypizza.domain.http-port:8080}")
@@ -17,7 +19,7 @@ public class HttpRedirectServer {
 
     @PostConstruct
     public void startRedirectServer() {
-        System.out.println("🚀 HTTP 리디렉션 서버가 시작됩니다. 포트: " + httpPort);
+        log.info("🚀 HTTP 리디렉션 서버가 시작됩니다. 포트: " + httpPort);
         HttpServer.create()
                 .port(httpPort)
                 .bindAddress(() -> new java.net.InetSocketAddress("0.0.0.0", httpPort))
