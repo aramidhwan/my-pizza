f8a528f0fbe493a4fe083be8026a2f52ac2e09d5
Shin, Seung Hwan <aramidhwan@naver.com>
2025-04-15 10:34:57 +0900

    [HttpRedirectServer] 수정

--- gateway/src/main/java/com/study/mypizza/gateway/config/HttpRedirectServer.java
+++ gateway/src/main/java/com/study/mypizza/gateway/config/HttpRedirectServer.java
@@ -11,28 +11,33 @@ import reactor.netty.http.server.HttpServer;
 @Slf4j
 public class HttpRedirectServer {
 
-    @Value("${mypizza.domain.http-port:8080}")
+    @Value("${mypizza.domain.redirect.enabled:false}")
+    private boolean redirect;
+
+    @Value("${mypizza.domain.redirect.http-port:8080}")
     private int httpPort;
 
-    @Value("${mypizza.domain.https-port:90}")
+    @Value("${mypizza.domain.redirect.https-port:90}")
     private int httpsPort;
 
     @PostConstruct
     public void startRedirectServer() {
-        log.info("🚀 HTTP 리디렉션 서버가 시작됩니다. 포트: " + httpPort);
-        HttpServer.create()
-                .port(httpPort)
-                .bindAddress(() -> new java.net.InetSocketAddress("0.0.0.0", httpPort))
-                .handle((req, res) -> {
-                    String host = req.requestHeaders().get("Host");
-                    String redirectHost = host.contains(":")
-                            ? host.replaceFirst(":\\d+", ":" + httpsPort)
-                            : host + ":" + httpsPort;
-                    String location = "https://" + redirectHost + req.uri();
-                    return res.status(301)
-                            .header("Location", location)
-                            .send();
-                })
-                .bindNow();
+        if ( redirect ) {
+            log.info("🚀 HTTP 리디렉션 서버가 시작됩니다. 포트: " + httpPort);
+            HttpServer.create()
+                    .port(httpPort)
+                    .bindAddress(() -> new java.net.InetSocketAddress("0.0.0.0", httpPort))
+                    .handle((req, res) -> {
+                        String host = req.requestHeaders().get("Host");
+                        String redirectHost = host.contains(":")
+                                ? host.replaceFirst(":\\d+", ":" + httpsPort)
+                                : host + ":" + httpsPort;
+                        String location = "https://" + redirectHost + req.uri();
+                        return res.status(301)
+                                .header("Location", location)
+                                .send();
+                    })
+                    .bindNow();
+        }
     }
 }
