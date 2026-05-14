b3bedee80d6f42dd72f9a924eab9cc696341ca78
aramidhwan <aramidhwan@naver.com>
2026-05-14 13:12:31 +0900

    실제 SQL 구문 코멘트 추가

--- common/src/main/java/com/study/mypizza/common/repository/CustomerRepository.java
+++ common/src/main/java/com/study/mypizza/common/repository/CustomerRepository.java
@@ -18,6 +18,12 @@ public interface CustomerRepository extends JpaRepository<Customer, Integer> {
     // JOIN FETCH는 일반 JOIN과 달리:
     //      1. 연관된 엔티티(authorities)를 즉시 로딩(eager loading) 해서 한 번의 쿼리로 가져옴.
     //      2. N+1 문제를 방지할 수 있음.
+    // 실제 SQL : select c1_0.customer_no,c1_0.activated,a1_0.customer_no,a1_1.authority_name,c1_0.customer_id,c1_0.customer_name,c1_0.email,c1_0.extra_roles,c1_0.password
+    //           from t_customer c1_0
+    //           join t_customer_authority a1_0
+    //                      on c1_0.customer_no=a1_0.customer_no
+    //           join t_authority a1_1
+    //                      on a1_1.authority_name=a1_0.authority_name
     @Query("SELECT c FROM Customer c JOIN FETCH c.authorities")
     List<Customer> findAllCustomers();
 
