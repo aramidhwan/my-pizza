c586cc9d215592f5d86f92987f5e5481d81eee6f
aramidhwan <aramidhwan@naver.com>
2026-07-18 21:38:33 +0900

    @JsonProperty에 설명 주석 추가

--- store/src/main/java/com/study/mypizza/store/dto/ResponseDto.java
+++ store/src/main/java/com/study/mypizza/store/dto/ResponseDto.java
@@ -18,7 +18,7 @@ public class ResponseDto {
     private String jwtAccessToken ;
     private Object data ;
 
-    @JsonProperty("BIZ_SUCCESS")
+    @JsonProperty("BIZ_SUCCESS")    /* camelCase와 외부 API/프론트의 snake_case가 다를 때 사용, Jackson이 Java 객체와 JSON을 변환할 때, JSON 필드명과 Java 필드명(또는 getter/setter)을 연결하는 어노테이션 */
     public int getBIZ_SUCCESS() {
         return BIZ_SUCCESS ;
     }
