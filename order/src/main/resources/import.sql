c2ae6486a55c82e61409247b9ecf26b3d133d072
aramidhwan <aramidhwan@naver.com>
2026-07-21 07:48:52 +0900

    구문오류 수정 (여러줄 --> 한줄)

--- order/src/main/resources/import.sql
+++ order/src/main/resources/import.sql
@@ -1,14 +1,13 @@
 -- 테이블 데이터 my-pizza.t_item:~12 rows (대략적) 내보내기
-INSERT INTO `t_item` (`item_id`, `item_group`, `item_nm`, `price_per_one`, `regist_dt`) VALUES
-	(1, '피자', '페파로니피자', 6000, '2024-04-02 12:05:10.887000'),
-	(2, '피자', '치즈피자', 4000, '2024-04-02 12:05:10.998000'),
-	(3, '피자', '고구마피자', 6000, '2024-04-02 12:05:11.001000'),
-	(4, '스파게티', '볼레로스파게티', 4000, '2024-04-02 12:05:11.003000'),
-	(5, '스파게티', '까르보나라스파게티', 4000, '2024-04-02 12:05:11.006000'),
-	(6, '스파게티', '알리오스파게티', 4000, '2024-04-02 12:05:11.008000'),
-	(7, '분식', '로제떡볶이', 3000, '2024-04-02 12:05:11.012000'),
-	(8, '분식', '소떡소떡', 2500, '2024-04-02 12:05:11.014000'),
-	(9, '분식', '쫄깃닭꼬치', 2500, '2024-04-02 12:05:11.018000'),
-	(10, '음료', '펩시콜라', 1000, '2024-04-02 12:05:11.023000'),
-	(11, '음료', '코카콜라', 1000, '2024-04-02 12:05:11.028000'),
-	(12, '음료', '환타', 1000, '2024-04-02 12:05:11.032000');
+INSERT INTO `t_item` (`item_id`, `item_group`, `item_nm`, `price_per_one`, `regist_dt`) VALUES (1, '피자', '페파로니피자', 6000, '2024-04-02 12:05:10.887000');
+INSERT INTO `t_item` (`item_id`, `item_group`, `item_nm`, `price_per_one`, `regist_dt`) VALUES (2, '피자', '치즈피자', 4000, '2024-04-02 12:05:10.998000');
+INSERT INTO `t_item` (`item_id`, `item_group`, `item_nm`, `price_per_one`, `regist_dt`) VALUES (3, '피자', '고구마피자', 6000, '2024-04-02 12:05:11.001000');
+INSERT INTO `t_item` (`item_id`, `item_group`, `item_nm`, `price_per_one`, `regist_dt`) VALUES (4, '스파게티', '볼레로스파게티', 4000, '2024-04-02 12:05:11.003000');
+INSERT INTO `t_item` (`item_id`, `item_group`, `item_nm`, `price_per_one`, `regist_dt`) VALUES (5, '스파게티', '까르보나라스파게티', 4000, '2024-04-02 12:05:11.006000');
+INSERT INTO `t_item` (`item_id`, `item_group`, `item_nm`, `price_per_one`, `regist_dt`) VALUES (6, '스파게티', '알리오스파게티', 4000, '2024-04-02 12:05:11.008000');
+INSERT INTO `t_item` (`item_id`, `item_group`, `item_nm`, `price_per_one`, `regist_dt`) VALUES (7, '분식', '로제떡볶이', 3000, '2024-04-02 12:05:11.012000');
+INSERT INTO `t_item` (`item_id`, `item_group`, `item_nm`, `price_per_one`, `regist_dt`) VALUES (8, '분식', '소떡소떡', 2500, '2024-04-02 12:05:11.014000');
+INSERT INTO `t_item` (`item_id`, `item_group`, `item_nm`, `price_per_one`, `regist_dt`) VALUES (9, '분식', '쫄깃닭꼬치', 2500, '2024-04-02 12:05:11.018000');
+INSERT INTO `t_item` (`item_id`, `item_group`, `item_nm`, `price_per_one`, `regist_dt`) VALUES (10, '음료', '펩시콜라', 1000, '2024-04-02 12:05:11.023000');
+INSERT INTO `t_item` (`item_id`, `item_group`, `item_nm`, `price_per_one`, `regist_dt`) VALUES (11, '음료', '코카콜라', 1000, '2024-04-02 12:05:11.028000');
+INSERT INTO `t_item` (`item_id`, `item_group`, `item_nm`, `price_per_one`, `regist_dt`) VALUES (12, '음료', '환타', 1000, '2024-04-02 12:05:11.032000');
