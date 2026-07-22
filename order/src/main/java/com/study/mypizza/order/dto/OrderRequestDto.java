cae78833259f689061e35a72ea4d22960855770b
aramidhwan <aramidhwan@naver.com>
2026-07-22 15:45:29 +0900

    불필요 로직 삭제

--- order/src/main/java/com/study/mypizza/order/dto/OrderRequestDto.java
+++ order/src/main/java/com/study/mypizza/order/dto/OrderRequestDto.java
@@ -8,45 +8,14 @@ import java.util.Map;
 
 @Builder
 @Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
 @Slf4j
 public class OrderRequestDto {
-
-//    private Long orderId;
+    @Setter
     private int customerNo;
-//    private Long storeId;
     private String regionNm;
     private Integer totalPrice;
     private List<ItemDto> items;
-//    private List<ItemDto> itemDtos;
-//    private String status;
-//    private String statusInfo;
-//    private Date orderDt;
-
-//    public Order toEntity() {
-//        return Order.builder()
-//                .orderId(orderId)
-//                .customerId(customerId)
-//                .storeId(storeId)
-//                .status(status)
-//                .statusInfo(statusInfo)
-//                .regionNm(regionNm)
-//                .orderDt(orderDt)
-//                .build();
-//    }
-//
-//    public static OrderRequestDto of(Order order) {
-//        return OrderRequestDto.builder()
-//                .orderId(order.getOrderId())
-//                .customerId(order.getCustomerId())
-//                .storeId(order.getStoreId())
-//                .status(order.getStatus())
-//                .statusInfo(order.getStatus())
-//                .regionNm(order.getRegionNm())
-//                .orderDt(order.getOrderDt())
-//                .build();
-//    }
 }
