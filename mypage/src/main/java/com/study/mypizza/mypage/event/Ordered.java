0160a89fe5d5f056bfa932da0722811ad85e4858
aramidhwan <aramidhwan@naver.com>
2026-07-24 07:50:39 +0900

    불필요 import 삭제, 이벤트객체에서 불필요 createDt, updateDt 컬럼 삭제

--- mypage/src/main/java/com/study/mypizza/mypage/event/Ordered.java
+++ mypage/src/main/java/com/study/mypizza/mypage/event/Ordered.java
@@ -1,14 +1,8 @@
 package com.study.mypizza.mypage.event;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.study.mypizza.mypage.enums.OrderStatus;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-
-import java.time.LocalDateTime;
 
 @Data
 @EqualsAndHashCode(callSuper=false)
@@ -20,12 +14,6 @@ public class Ordered extends AbstractEvent {
     private String statusInfo;
     private String regionNm;
     private Integer totalPrice;
-    @JsonSerialize(using = LocalDateTimeSerializer.class)
-    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
-    private LocalDateTime createDt;
-    @JsonSerialize(using = LocalDateTimeSerializer.class)
-    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
-    private LocalDateTime updateDt;
 
     /**
      * 동일 aggregate의 이벤트 순서를 보장하기 위한 Kafka key.
