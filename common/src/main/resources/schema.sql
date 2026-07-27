27cb051cc70fef6c53458a6a87f9ff0a019a3b1a
aramidhwan <aramidhwan@naver.com>
2026-07-28 07:27:55 +0900

    fk명칭 변경

--- common/src/main/resources/schema.sql
+++ common/src/main/resources/schema.sql
@@ -21,8 +21,8 @@ CREATE TABLE t_customer (
 CREATE TABLE t_customer_authority (
   `customer_no` int NOT NULL,
   `authority_name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  KEY `FKtmaj6f4y7ig4q3rmd518au8ke` (`authority_name`),
-  KEY `FK7hoxi1jrqolgibyl3panktk2c` (`customer_no`),
-  CONSTRAINT `FK7hoxi1jrqolgibyl3panktk2c` FOREIGN KEY (`customer_no`) REFERENCES `t_customer` (`customer_no`),
-  CONSTRAINT `FKtmaj6f4y7ig4q3rmd518au8ke` FOREIGN KEY (`authority_name`) REFERENCES `t_authority` (`authority_name`)
+  KEY `fk_customerauthority_authority` (`authority_name`),
+  KEY `fk_customerauthority_customer` (`customer_no`),
+  CONSTRAINT `fk_customerauthority_customer` FOREIGN KEY (`customer_no`) REFERENCES `t_customer` (`customer_no`),
+  CONSTRAINT `fk_customerauthority_authority` FOREIGN KEY (`authority_name`) REFERENCES `t_authority` (`authority_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
